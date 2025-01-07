84a9faa93696b8dfdf4837bd78c6fdb09f88e927
amnajee-art <aftabamna13@gmail.com>
2025-01-07 23:05:46 +0500

    token issue

---
+++ src/app/api/blog/route.js
@@ -0,0 +1,38 @@
+// http://localhost:3000/api/blog
+
+import Blog from "../../../../models/Blog";
+import { connect } from "../../../../lib/db";
+import { NextRequest, NextResponse } from "next/server"
+import {verifyJwtToken} from "../../../../lib/jwt"
+
+
+export async function POST(req) {
+    await connect();
+
+    const accessToken = req.headers.get("authorization")
+    console.log(accessToken)
+    const token = accessToken.split(" ")[1];
+    console.log(token)
+
+    const decodedToken =  verifyJwtToken(token);
+
+
+    if(!accessToken || !decodedToken){
+        return new Response (
+            JSON.stringify({error: "Unauthorized wrong or expired token"}),
+            {status:403}
+        )
+    }
+
+    try{
+        const body = await req.json();
+        const newBlog = await Blog.create(body);
+
+        return NextResponse.json(newBlog, {status:201})
+
+    }catch(error){
+
+        return NextResponse.json({message: "POST Error (create blog)"})
+
+    }
+}
